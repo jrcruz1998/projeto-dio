dd83a04ebdf541d03495b11346ddc862090ede79
Jonathas <jonathas-cruz@hotmail.com>
2023-09-26 21:18:04 -0300

    Commit desafio alunos do curso Entendendo a Estrutura e Depurando Apps Android DIO

---
+++ app/src/main/java/com/example/suporteavariosidiomas/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.suporteavariosidiomas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // O desafio será criar um valor dentro do string.xml
+        // e trocar o texto do xml e tornar internacional (qualquer linguagem)
+    }
+}
